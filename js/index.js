a114368f54f05f6e851c08d16011d7b11fdb2790
8264 <616658975@qq.com>
2016-11-30 01:29:03 +0800

    this is baiduyuqing

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".part").mousedown(function(e){
+        e.preventDefault();
+    });
+    $(".part").mousemove(function(e){
+        e.preventDefault();
+    });
+    touch.on("body","swipeup",".part",function(){
+        
+    })
+})
